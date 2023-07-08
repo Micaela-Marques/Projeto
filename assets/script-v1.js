3576890ed3c3ae6170686a85e1485ddbd85c4be6
Micaela Marques <micaellagrangeiro@gmail.com>
2023-07-08 17:06:40 -0300

    : tada:initial commit

---
+++ assets/script-v1.js
@@ -0,0 +1,14 @@
+function toggleMode() {
+ const html= document.documentElement
+ if (html.classList.contains("light")) {
+   
+ } else {
+  
+ }
+}
+  
+//pegar a tag img
+
+//substituir a imagem
+// se tiver light mode, adicionar a imagem light
+// se tiver sem o light mode, manter a imagem normal
